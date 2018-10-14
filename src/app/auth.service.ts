0278d24c8433a3123b7d58ce2a84a884e250910e
marques002 <netoguitars@gmail.com>
2018-10-13 22:46:54 -0300

    Logicas de logn,cadastro,adicionar tarefas e excluir aplicadas, restam as de adicionar datas e editar.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,13 @@ import { User } from './models/user';
 
 @Injectable()
 export class AuthService {
+  findAll(): string[]{
+    if(localStorage.getItem('tarefasList') != null)
+    {
+      return JSON.parse(localStorage.getItem('tarefasList'));
+    }
+    return null;
+  }
 
   private users : Array<User> = [
 
@@ -23,5 +30,31 @@ export class AuthService {
   //método para adicinar objeto ao array
   add(newUser){
     this.users.push(newUser)
+  }
+  addTarefa(title : string): void{
+
+    if(localStorage.getItem('tarefasList') == null){
+
+      let list: string[] = [];
+
+      list.push(title);
+
+      localStorage.setItem('tarefasList', JSON.stringify(list));
+
+    }else{
+      let list: string[] = JSON.parse(localStorage.getItem('tarefasList'));
+      list.push(title);
+      localStorage.setItem('tarefasList', JSON.stringify(list));
+    }
+
+  }
+
+  
+
+  deletTarefa(index: number): void{
+    let list: string[] = JSON.parse(localStorage.getItem('tarefasList'));
+    list.splice(index, 1);
+    localStorage.setItem('tarefasList', JSON.stringify(list));
+
   }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,8 +3,9 @@ import { LoginComponent } from './login/login.component';
 import { DashComponent } from './dash/dash.component';
 import { RegisterComponent } from './register/register.component';
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+    
     { path: 'dash', component: DashComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' }
 ]
